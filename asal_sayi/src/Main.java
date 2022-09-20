467858af298e5c0b9e62467204096a1a50d86897
Hakan CERAN <hakanceran64@gmail.com>
2022-09-20 23:17:06 +0300

    Asal sayı

---
+++ asal_sayi/src/Main.java
@@ -0,0 +1,36 @@
+/*
+ * @Authors: Hakan CERAN
+ * @Date: 20.09.2022
+ * @Content: Asal sayı
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 127;
+
+        int remainder = number % 2;
+
+        boolean isPrime = true;
+
+        if(number == 1){
+            System.out.println("Sayı asal değildir.");
+            return;
+        }
+
+        if(number < 1){
+            System.out.println("Geçersiz sayi.");
+        }
+
+        for(int i = 2; i < number; i++){
+            if(number % i == 0){
+                isPrime = false;
+            }
+        }
+
+        if(isPrime){
+            System.out.println("Sayı asaldır.");
+        } else {
+            System.out.println("Sayı asal değildir.");
+        }
+    }
+}
